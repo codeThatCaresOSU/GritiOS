8d42487885831eb407d3f91cc79eb225c51f01da
Jared <williamsjared62@gmail.com>
2018-02-23 12:07:08 -0500

    Nothing

--- Grit/Controller/ProfileController.swift
+++ Grit/Controller/ProfileController.swift
@@ -9,20 +9,60 @@
 import UIKit
 
 class ProfileController: UIViewController {
+    
+    
+    
+    private lazy var mainScrollView: UIScrollView = {
+        let view = UIScrollView(frame: self.view.frame)
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private lazy var signUpButton: UIButton = {
+       let button = UIButton()
+        
+        
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
+
     }
     
+
+    
     func setupView() {
+        self.tabBarController?.tabBarItem.title = "Profile"
+        self.navigationController?.navigationBar.topItem?.title = "Profile"
         self.view.backgroundColor = .white
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.resignKeyboard)))
+        
+        login()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    @objc func resignKeyboard() {
+        self.view.subviews.forEach() {
+            if $0 is UIScrollView {
+                $0.subviews.forEach() {
+                    if $0 is UITextField {
+                        $0.resignFirstResponder()
+                    }
+                }
+            }
+        }
     }
-
-
+    
+    func login() {
+        Utility.createTextFieldsAndConstrainToView(view: self.mainScrollView, placeholders: ["First Name", "Last Name", "Zip Code", "Email", "Password"])
+        self.view.addSubview(self.mainScrollView)
+    }
+    
+    func profile() {
+        
+    }
 }
 
