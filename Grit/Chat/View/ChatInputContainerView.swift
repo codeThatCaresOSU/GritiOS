9eedba419711804bba9bfdd1e266779ec42ded3c
Frank_Cerny <frankk.cerny@gmail.com>
2018-03-28 17:27:54 -0400

    Adding initial chat files

---
+++ Grit/Chat/View/ChatInputContainerView.swift
@@ -0,0 +1,9 @@
+//
+//  ChatInputContainerView.swift
+//  Grit
+//
+//  Created by Frank  on 3/28/18.
+//  Copyright © 2018 Jared Williams. All rights reserved.
+//
+
+import Foundation
---
+++ Grit/Chat/View/UserCell.swift
@@ -0,0 +1,9 @@
+//
+//  UserCell.swift
+//  Grit
+//
+//  Created by Frank  on 3/28/18.
+//  Copyright © 2018 Jared Williams. All rights reserved.
+//
+
+import Foundation
