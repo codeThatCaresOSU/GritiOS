bb09c8db9b4ba7e8e7c3dc4232c0a29e209be691
Frank_Cerny <frankk.cerny@gmail.com>
2018-04-07 22:33:08 -0400

    Implementing different collection view cells for chat

---
+++ Grit/Chat/View/FindMentorCell.swift
@@ -0,0 +1,9 @@
+//
+//  FindMentorCell.swift
+//  Grit
+//
+//  Created by Frank  on 4/7/18.
+//  Copyright © 2018 Jared Williams. All rights reserved.
+//
+
+import Foundation
---
+++ Grit/Chat/View/MessageFillerCell.swift
@@ -0,0 +1,9 @@
+//
+//  MessageFillerCell.swift
+//  Grit
+//
+//  Created by Frank  on 4/7/18.
+//  Copyright © 2018 Jared Williams. All rights reserved.
+//
+
+import Foundation
